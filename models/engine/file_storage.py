8d4c43da7ce67af99e3b0e73e87b6e76f9a5d19c
Richard Sim <rdsim8589@gmail.com>
2017-02-08 19:54:46 -0800

    created file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,28 +10,34 @@ class FileStorage:
     """
     Serializes instance to JSON and deserialize JSON file to an instance
     """
-    __path = "file.json"
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ returns FileStorage.__.objects"""
+        """ returns FileStorage.__objects"""
         return FileStorage.__objects
 
     def new(self, obj):
         """ create """
-        FileStorage.__objects[obj.id] = obj
+        FileStorage.__objects.update({obj.id:obj})
 
     def save(self):
-        """ serializes FileStorage.__.objects"""
+        """ serializes FileStorage.__objects"""
         with open(FileStorage.__file_path, mode='w', encoding='utf-8') as jsonFile:
-            jsonFile.write(json.dumps(FileStorage.__objects))
+            tmp_dict = {}
+            for key in FileStorage.__objects.keys():
+                tmp_dict[key] = FileStorage.__objects[key].to_json()
+            jsonFile.write(json.dumps(tmp_dict))
 
     def reload(self):
-        """deserializes FileStorage.__.objects """
-        if os.path.isfile(FileStorage.__file_path) == True:
+        """deserializes FileStorage.__objects """
+        from models.base_model import BaseModel
+        if os.path.isfile(FileStorage.__file_path):
             try:
-                with open(FileStorage.__file_path, mode='r', encoding='utf-8') as jsonFile:
-                    
-                    FileStorage.__objects = json.loads(jsonFile.read())
+                with open(FileStorage.__file_path, mode='r', encoding='utf-8') as jFile:
+                    obj = json.load(jFile)
+                    for key in obj.keys():
+                        new_obj = BaseModel(obj[key])
+                        FileStorage.__objects[key] = new_obj
             except Exception as e:
-                pass
+                print(e)
