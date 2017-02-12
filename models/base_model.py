32bb11b3e0c118152847e05a784fadcdbe0be433
guptaNswati <sgupta1505@gmail.com>
2017-02-12 20:27:49 +0000

    updated tests for console.py

--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,7 @@ class BaseModel:
             # make a random UUID
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             storage.new(self)
 
     """
