2e14f65ad570ab58f6da7a255998c083a1de06e6
Richard Sim <rdsim8589@gmail.com>
2017-03-15 13:43:36 -0700

    added comments and restart nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
-# setup web server for deplyment of web_static
+# check if package is installed
 dpkg -s nginx
 if [ "$echo $?" -ne 0 ]; then
     apt-get update
     sudo apt-get -y install nginx
 fi
+# creates the directories of the two given paths
 DIR="/data/web_static/releases/test /data/web_static/shared/"
 for dir in $DIR; do
     if [ ! -d "$dir" ]; then
 	sudo mkdir -p "$dir"
     fi
 done
+# push a test index file into test
 FILE="/data/web_static/releases/test/index.html"
 if [ ! -f $FILE ]; then
     echo '<html>
@@ -21,9 +23,13 @@ if [ ! -f $FILE ]; then
   </body>
 </html>' | sudo tee $FILE
 fi
+# creates a symbolic link
 SYMBOLIC="/data/web_static/current"
 sudo ln -fs /data/web_static/releases/test $SYMBOLIC
 
+# recursively change the owner and group
 DIR="/data/"
 sudo chown -R ubuntu:ubuntu $DIR
-sed -i '37i \\tlocation /hbnb_static/ \{\n\t\talias /data/web_static/current/;\n\t\tindex index.html index.htm;\n\t\tautoindex on;\n\t\}' /etc/nginx/sites-available/default
+# configure to serve /hbnb_static
+sudo sed -i '37i \\tlocation /hbnb_static/ \{\n\t\talias /data/web_static/current/;\n\t\tindex index.html index.htm;\n\t\tautoindex on;\n\t\}' /etc/nginx/sites-available/default
+sudo service nginx restart
