6422526cd164ac2a9aec4ae475f20821905bd29f
Richard Sim <rdsim8589@gmail.com>
2017-03-15 15:44:57 -0700

    able to use fabric to create versions and tarz the file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+this module contains the method do_pack
+"""
+from fabric.api import local, task
+import os
+
+
+@task
+def do_pack():
+    """
+    creates a version_folder
+    compresses all files found in web_static
+    """
+    if not os.path.exists('versions'):
+        os.mkdir('versions')
+    try:
+        local('sudo tar -zcf web_static_$(date +%Y%m%d%H%M%S).tgz ./versions')
+        return (os.path.abspath('./versions'))
+    except:
+        return(None)
+
+
