0cff4801f54e81bef02178c3c3853a2a6ea6b5f0
Richard Sim <rdsim8589@gmail.com>
2017-03-15 22:57:40 -0700

    added the do_pack and more to the description

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
 module contains the deploy
+use case
+fab -f 2-do_deploy_web_static.py do_deploy:archive_path=<tar file> /
+    -i <ssh key> -u ubuntu
 """
 import os
 from fabric.api import put, sudo, env, task
@@ -27,8 +30,31 @@ def do_deploy(archive_path):
         sudo('rm -rf {}/web_static'.format(tar_path))
         sudo('rm -rf /data/web_static/current')
         sudo('ln -s {} /data/web_static/current'.format(tar_path))
-
         print('New version deployed!')
         return(True)
     except:
         return(False)
+
+
+"""
+this module contains the method do_pack
+fab -f 1-pack_web_static.py do_deploy:archive_path=<tar file> /
+    -i <ssh key> -u ubuntu
+"""
+
+
+@task
+def do_pack():
+    """
+    creates a version_folder
+    compresses all files found in web_static
+    """
+    if not os.path.exists('versions'):
+        os.mkdir('versions')
+    try:
+        tar_time = local('date +%Y%m%d%H%M%S', capture=True)
+        local('run tar -zcf ./versions/web_static_' + tar_time + '.tgz \
+        ./web_static')
+        return (os.path.abspath('./versions/web_static_' + tar_time + '.tgz'))
+    except:
+        return(None)
