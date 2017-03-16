c7c5a8cc510955dde5802bf5d89acc6819452624
Richard Sim <rdsim8589@gmail.com>
2017-03-15 17:30:41 -0700

    created the do_deploy mmodule

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,29 @@
+#!/bin/usr/python3
+"""
+module contains the deploy
+"""
+import os
+from fabric.api import put, run, env
+
+
+def do_deploy(archive_path):
+    """
+    distrubes the archives to webservers
+    """
+    if not os.path.exists(archive_path):
+        return (False)
+    try:
+        archive_name = archive_path.split('/')[-1].split('.')[0]
+        put(archive_path, '/tmp/')
+        run('sudo mkdir -p mkdir -p /data/web_static/releases/' + archive_name)
+        run('sudo tar -xzf /tmp/' + archive_name +
+            '.tgz -C /data/web_static/releases/' + archive_name)
+        run('sudo rm /tmp/' + archive_name + '.tgz')
+        run('mv /data/web_static/releases/' + archive_name +
+            '/web_static/* /data/web_static/releases/' + archive_name + '/')
+        run('rm -rf /data/web_static/releases/' + archive_name + '/web_static')
+        run('rm -rf /data/web_static/current')
+        run(' ln -s /data/web_static/releases/' + archive_name +
+            '/ /data/web_static/current')
+    except:
+            return(False)
