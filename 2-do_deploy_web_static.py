a90f7bf197f1062045dc113f7fb23e8b6d1f0aad
Richard Sim <rdsim8589@gmail.com>
2017-03-15 19:13:06 -0700

    check to see if a file exist

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,7 +14,7 @@ def do_deploy(archive_path):
     """
     distrubes the archives to webservers
     """
-    if not os.path.exists(archive_path):
+    if not os.path.isfile(archive_path):
         return (False)
     try:
         archive_name = archive_path.split('/')[-1].split('.')[0]
